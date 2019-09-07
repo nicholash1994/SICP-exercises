0c1aca299cbcfa01f7ae73ac75185da4a317c451
Nick <nicholashannani@yahoo.com>
2019-09-07 02:48:14 -0400

    Fixed exercise 1.11
    The iterative process was called a recursive process and vice-versa

--- exercise-1.11.lisp
+++ exercise-1.11.lisp
@@ -1,10 +1,10 @@
-; iterative process
+; recursive process
 (define (f n)
     (if (< n 3)
         n
         (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
 
-; recursive process
+; iterative process
 (define (f2 n)
     (define (iter a b c count)
         (if (= count n)
